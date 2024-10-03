2125b2d2b7cf51d1937dc9d835bfd6f8161d7cb5
Jørgen Vatle <jorgen@wcaservices.net>
2024-10-03 14:56:00 +0300

    Add option to override the version to build

--- versions.sh
+++ versions.sh
@@ -5,3 +5,9 @@ meteor_versions=( \
   '3.0-rc.2' \
   '3.0-rc.4'
 )
+
+# If METEOR_VERSION is defined
+if [ -n "$METEOR_VERSION" ]; then
+  # Only build the specified version
+  meteor_versions=("$METEOR_VERSION")
+fi
